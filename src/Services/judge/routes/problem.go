17cfdc1b1decdbe93c316c37bdb979cf435a88bc
Chung Dinh Hoang <hgchung.dinh@gmail.com>
2025-07-10 23:41:53 +0700

    Add p

--- src/Services/judge/routes/problem.go
+++ src/Services/judge/routes/problem.go
@@ -2,20 +2,28 @@ package routes
 
 import (
 	"fmt"
+	"judge/storage"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func ProblemRoute(router fiber.Router) {
 	router.Post("/add", func(c *fiber.Ctx) error {
-		file, err := c.FormFile("file")
+		var problemId, packageId int
+		var err error
+
+		problemId, err = strconv.Atoi(c.Query("problemId", ""))
 		if err != nil {
-			return c.Status(400).SendString("Failed to read file")
+			return err
+		}
+		packageId, err = strconv.Atoi(c.Query("packageId", ""))
+		if err != nil {
+			return err
 		}
 
-		destination := fmt.Sprintf("/upload/%s", file.Filename)
-		if err = c.SaveFile(file, destination); err != nil {
-			return c.Status(400).SendString("Failed to save file")
+		if err := storage.AddProblem(uint64(problemId), uint64(packageId)); err != nil {
+			return c.SendString(fmt.Sprintf("Got error: %s", err.Error()))
 		}
 
 		return c.SendStatus(200)
