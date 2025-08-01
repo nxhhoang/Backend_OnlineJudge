e9c63b6edb7dab60ae1e9980b814a59ea03b7d52
Chung Dinh Hoang <hgchung.dinh@gmail.com>
2025-08-01 21:46:50 +0700

    Add getLatestPackageId() function

---
+++ src/Services/problem/utils/polygon/getLatestPackageId.go
@@ -0,0 +1,68 @@
+package polygon
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"problem/models"
+	"strconv"
+	"time"
+)
+
+// type packages struct {
+// 	pkgs []models.Package
+// }
+
+type returnStruct struct {
+	Status string           `json:"status"`
+	Result []models.Package `json:"result"`
+}
+
+func GetLastestPackage(problemId uint64) (uint64, error) {
+	resp, err := polygonApiCall("problem.packages", map[string]string{
+		"problemId": strconv.Itoa(int(problemId)),
+		"apiKey":    os.Getenv("POLYGON_API_KEY"),
+		"time":      fmt.Sprintf("%d", time.Now().Unix()),
+	})
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return 0, fmt.Errorf("Get error while getting latest packageId: %s", resp.Body)
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return 0, err
+	}
+
+	fmt.Println(string(body))
+
+	var packageId uint64
+
+	var return_json returnStruct
+	if err := json.Unmarshal(body, &return_json); err != nil {
+		return 0, err
+	}
+
+	for _, pkg := range return_json.Result {
+		fmt.Printf("%v\n", pkg)
+		if pkg.State != "READY" {
+			continue
+
+		}
+		if pkg.Type == "linux" {
+			continue
+		}
+
+		return pkg.ID, nil
+	}
+
+	return 0, fmt.Errorf("no valid packages found")
+
+	// fmt.Printf("%v\n", return_json)
+
+	return packageId, nil
+}
