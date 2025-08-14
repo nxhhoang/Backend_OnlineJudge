fb3a17a5b3ee0d25a1ed542b8db7e47bfe5fefe6
Chung Dinh Hoang <hgchung.dinh@gmail.com>
2025-08-14 20:45:37 +0700

    Update Contestant model
    This add contestant points for each problems and total points

--- src/Services/contest/domain/entity/contestant.go
+++ src/Services/contest/domain/entity/contestant.go
@@ -3,9 +3,11 @@ package domain
 import "time"
 
 type Contestant struct {
-	UserID    uint64    `bson:"user_id"`
-	Points    uint64    `bson:"points"`
-	RealStart time.Time `bson:"real_start"`
+	UserID    uint64    `bson:"user-id"`
+	RealStart time.Time `bson:"real-start"`
 
 	Submissions []uint64 `bson:"submissions"`
+
+	TotalPoints uint64   `bson:"total-points"`
+	Points      []uint64 `bson:"points"`
 }
